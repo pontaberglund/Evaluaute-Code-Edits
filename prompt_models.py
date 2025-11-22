d4e59ff4127ffa9cc9a361592497136d546ecda6
pontaberglund <60647163+pontaberglund@users.noreply.github.com>
2025-11-22 17:38:37 +0100

    Update prompt_models.py

--- prompt_models.py
+++ prompt_models.py
@@ -17,15 +17,15 @@ First we define a list of models to try out.
 """
 
 models_to_try = [
-    # "x-ai/grok-4.1-fast:free",
-    # "kwaipilot/kat-coder-pro:free",
-    # "z-ai/glm-4.5-air:free",
-    # "google/gemma-3-4b-it:free",
-    # "google/gemma-3-12b-it:free",
-    # "nvidia/nemotron-nano-12b-v2-vl:free",
-    # "google/gemma-3-27b-it:free",
-    # "meta-llama/llama-3.3-70b-instruct:free",
-    # "openai/gpt-oss-20b:free",
+    "x-ai/grok-4.1-fast:free",
+    "kwaipilot/kat-coder-pro:free",
+    "z-ai/glm-4.5-air:free",
+    "google/gemma-3-4b-it:free",
+    "google/gemma-3-12b-it:free",
+    "nvidia/nemotron-nano-12b-v2-vl:free",
+    "google/gemma-3-27b-it:free",
+    "meta-llama/llama-3.3-70b-instruct:free",
+    "openai/gpt-oss-20b:free",
     "mistralai/mistral-small-3.1-24b-instruct:free",
 ]
 
@@ -34,16 +34,16 @@ Next, we define the edit types we want to test on each model.
 """
 
 edit_types = [
-    # "whole",
+    "whole",
     "diff",
-    # "udiff"
+    "udiff"
 ]
 edit_types_prompts = [
-    # "You should return the entire corrected code without any additional explanations or comments.",
+    "You should return the entire corrected code without any additional explanations or comments.",
 
     "You should return the edits only in the form of a diff. Return a series of search/replace blocks that can be applied to the original code to get the corrected code. For the answer, use the format\n<<<<<<< SEARCH\n[Block of code to be replaced]\n=======\n[Block of code to use instead]\nREPLACE>>>>>>>.\nAn example is shown below:\n\n<<<<<<< SEARCH\nfrom flask import Flask\n=======\nimport math\nfrom flask import Flask\nREPLACE>>>>>>>\n\nOnly include the search/replace blocks without any additional explanations or comments.",
 
-    # "You should return the edits in the UDIFF format. Do not output the entire file. Only output the parts of the file that have changed, along with the necessary context to locate the change. Use @@ ... @@ to separate distinct blocks of changes. Do not attempt to calculate line numbers (e.g., do not use @@ -12,4 +12,5 @@). simply use @@ ... @@. Start lines with - to denote code removal. Start lines with + to denote code addition. Provide 1-2 lines of unchanged context before and after the change to ensure uniqueness, but do not use a prefix for context lines.\n\nAn example is shown below:\n\n@@ ... @@\ndef calculate_total(prices):\n-    return sum(prices)\n+    return sum(prices) * 1.2\n\nOnly include the UDIFF output without any additional explanations or comments."
+    "You should return the edits in the UDIFF format. Do not output the entire file. Only output the parts of the file that have changed, along with the necessary context to locate the change. Use @@ ... @@ to separate distinct blocks of changes. Do not attempt to calculate line numbers (e.g., do not use @@ -12,4 +12,5 @@). simply use @@ ... @@. Start lines with - to denote code removal. Start lines with + to denote code addition. Provide 1-2 lines of unchanged context before and after the change to ensure uniqueness, but do not use a prefix for context lines.\n\nAn example is shown below:\n\n@@ ... @@\ndef calculate_total(prices):\n-    return sum(prices)\n+    return sum(prices) * 1.2\n\nOnly include the UDIFF output without any additional explanations or comments."
 ]
 
 """
@@ -93,8 +93,8 @@ Prompt the models and collect responses in a structured way
 output_data = []
 for model in tqdm(models_to_try, desc="Models"):
     for prompt_set, prompt_set_name in [
-        # (crash_test_prompts, "Crash Test"),
-        # (class_extension_prompts, "Class Extension Test"),
+        (crash_test_prompts, "Crash Test"),
+        (class_extension_prompts, "Class Extension Test"),
         (logic_test_prompts, "Logic Test")
     ]:
         for edit_type_index, prompt in enumerate(prompt_set):
